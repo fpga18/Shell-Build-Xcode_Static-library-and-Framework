b16069701f284f1ce1b4e2c82a456ee9e22dcee2
fpga18 <413045950@qq.com>
2015-06-26 09:48:07 +0800

    Update BuildXcodeShell.sh

--- BuildXcodeShell.sh
+++ BuildXcodeShell.sh
@@ -33,17 +33,17 @@ cd $TARGETPATH
 xcodebuild -target $TARGETNAME -configuration Release -sdk iphonesimulator -arch i386 -arch x86_64
 xcodebuild -target $TARGETNAME -configuration Release -sdk iphoneos -arch armv7 -arch armv7s -arch arm64
 LIBTOOL_FLAGS="-static"
-FWNAME="MAASDK"
+FWNAME="ASDK"
 RELEASEIPHONEA="$TARGETPATH/build/Release-iphoneos"
 RELEASESIMULATORA="$TARGETPATH/build/Release-iphonesimulator"
 STATICLIBNAME="lib$TARGETNAME.a"
 HEADPATH=""
-echo "Creating MAASDK.framework"
-if [ -d "MAASDK.framework" ]; then
-echo "Removing previous MAASDK.framework copy"
-rm -rf "MAASDK.framework"
+echo "Creating ASDK.framework"
+if [ -d "ASDK.framework" ]; then
+echo "Removing previous ASDK.framework copy"
+rm -rf "ASDK.framework"
 fi
-mkdir -p "MAASDK.framework/Headers"
+mkdir -p "ASDK.framework/Headers"
 libtool -no_warning_for_no_symbols $LIBTOOL_FLAGS -o $FWNAME.framework/$FWNAME $RELEASEIPHONEA/$STATICLIBNAME $RELEASESIMULATORA/$STATICLIBNAME
 cp -r $RELEASEIPHONEA/"include/$TARGETNAME"/* $FWNAME.framework/Headers/
 echo "Created $FWNAME.framework"
